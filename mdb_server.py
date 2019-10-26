cb415b788799c3e2972a3919b979bc71f8cfa513
Ken Hoover <khooveri@eng.ucsd.edu>
2019-10-25 20:58:24 -0700

    Added more Peripheral asbtract methods.

--- mdb_server.py
+++ mdb_server.py
@@ -10,26 +10,46 @@ logger = logging.getLogger(__name__)
 
 
 class Peripheral(ABC):
+    """Generic implementation of an MDB peripheral interface."""
     def __init__(self):
         self.lock = asyncio.Lock()
         self.initialized = False
 
     @abstractmethod
     async def initialize(self, master, send_reset=True):
+        """Initialization code for the peripheral.
+
+        Feel free to send messages here, they won't actually be sent until the
+        USB connection is established.
+
+        :param master: the Master instance controlling this peripheral
+        :param send_reset: whether to send a reset command or not"""
         logger.debug("Initializing peripheral of type "
                      f"{self.__class__.__name__}.")
         self.initialized = True
         self.master = master
 
     async def send(self, message):
-        await self.master.send(message)
+        async with self.lock:
+            await self.master.send(message)
+
+    async def sendread(self, message, prefix):
+        async with self.lock:
+            return await self.master.sendread(message, prefix)
 
     @abstractmethod
     async def enable(self):
+        """Enables the peripheral for vending activities."""
         pass
 
     @abstractmethod
     async def disable(self):
+        """Disables the peripheral for vending activities."""
+        pass
+
+    @abstractmethod
+    async def run(self):
+        """Does whatever persistent action is needed."""
         pass
 
 
