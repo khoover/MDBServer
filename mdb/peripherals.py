491ce756cdd6f43bac0fa61ac7c28dde730e7252
Ken Hoover <khooveri@eng.ucsd.edu>
2019-10-27 16:51:34 -0700

    Removed the master import in peripherals.
    It was causing circular dependencies. Also added non-locking versions
    of send and sendread to the peripherals, for when the caller might
    want all mdb messages to occur without interruption.

--- mdb/peripherals.py
+++ mdb/peripherals.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 import asyncio
 import logging
-from . import Master
 
 logger = logging.getLogger(__name__)
 
@@ -9,14 +8,13 @@ logger = logging.getLogger(__name__)
 class Peripheral(ABC):
     """Generic implementation of an MDB peripheral interface."""
     lock: asyncio.Lock
-    master: Master
 
     def __init__(self):
         self.lock = asyncio.Lock()
         self.initialized = False
 
     @abstractmethod
-    async def initialize(self, master: Master, send_reset=True):
+    async def initialize(self, master, send_reset=True) -> None:
         """Initialization code for the peripheral.
 
         Feel free to send messages here, the USB interface will be initialized
@@ -29,28 +27,40 @@ class Peripheral(ABC):
         self.initialized = True
         self.master = master
 
-    async def send(self, message: str):
+    async def send(self, message: str) -> None:
         assert self.initialized
         async with self.lock:
             await self.master.send(message)
 
-    async def sendread(self, message: str, prefix: str):
+    async def send_nolock(self, message: str) -> None:
+        """Sends a message without acquiring the peripheral's lock. Assumes the
+        caller or a parent has acquired the lock. Must not be used to
+        circumvent the locking mechanism."""
+        assert self.initialized and self.lock.locked()
+        await self.master.send(message)
+
+    async def sendread(self, message: str, prefix: str) -> str:
         assert self.initialized
         async with self.lock:
             return await self.master.sendread(message, prefix)
 
+    async def sendread_nolock(self, message: str, prefix: str) -> str:
+        """Similar to send_nolock, except for sendread."""
+        assert self.initialized and self.lock.locked()
+        return await self.master.sendread(message, prefix)
+
     @abstractmethod
-    async def enable(self):
+    async def enable(self) -> None:
         """Enables the peripheral for vending activities."""
         assert self.initialized
 
     @abstractmethod
-    async def disable(self):
+    async def disable(self) -> None:
         """Disables the peripheral for vending activities."""
         assert self.initialized
 
     @abstractmethod
-    async def run(self):
+    async def run(self) -> None:
         """Does whatever persistent action is needed."""
         assert self.initialized
 
