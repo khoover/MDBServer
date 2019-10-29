f9c5764ba6c70475bec967a32fff21f3ca0c56b6
Ken Hoover <khooveri@eng.ucsd.edu>
2019-10-28 18:42:48 -0700

    Finished implementing the bill validator reset.
    Just need to figure out what the standard means when it gives the
    bitvector definition for bill enabling; if I want to enable bills 15 and
    3, do I send 8008H, 0880H, 0110H, or 1001H? All of these make sense,
    depending on how you read the standard. My intuition says 8008, in which
    case all that happens is a reverse before the shift-and-add, but 0110
    can also make sense, in which case you reverse, split the list into two
    pieces, and reverse each piece.

--- mdb/peripherals.py
+++ mdb/peripherals.py
@@ -8,6 +8,17 @@ logger = logging.getLogger(__name__)
 SETUP_TIME_SECONDS = 0.2
 
 
+class NonResponseError(Exception):
+    """Raised when a peripheral does not respond by the non-response deadline.
+
+    Attributes:
+        command -- The command that was supposed to be sent to the peripheral.
+        peripheral -- The peripheral the command was being sent to."""
+    def __init__(self, command, peripheral):
+        self.command = command
+        self.peripheral = peripheral
+
+
 class Peripheral(ABC):
     """Generic implementation of an MDB peripheral interface."""
     lock: asyncio.Lock
@@ -59,26 +70,28 @@ class Peripheral(ABC):
     # Next are utility methods so that implementations only have to worry about
     # handling the reset after the non-response timeout.
     async def sendread_until_timeout(self, message: str) -> str:
+        non_response_reply = self.BOARD_RESPONSE_PREFIX + ',NACK'
         message_status = await self.sendread(message)
         start = time.time()
-        while message_status == self.BOARD_RESPONSE_PREFIX + ',NACK' and \
+        while message_status == non_response_reply and \
                 time.time() - start < self.NON_RESPONSE_SECONDS:
             asyncio.sleep(self.POLLING_INTERVAL_SECONDS)  # Ratelimiting
             message_status = await self.sendread(message,
                                                  self.BOARD_RESPONSE_PREFIX)
-        # If the timeout is exceeded and we're still getting NACKs, let the
-        # caller know so they can do recovery.
+        if message_status == non_response_reply:
+            raise NonResponseError(message, self.__class__.__name__)
         return message_status
 
     async def sendread_nolock_until_timeout(self, message: str) -> str:
+        non_response_reply = self.BOARD_RESPONSE_PREFIX + ',NACK'
         message_status = await self.sendread_nolock(message)
         start = time.time()
         while message_status == self.BOARD_RESPONSE_PREFIX + ',NACK' and \
                 time.time() - start < self.NON_RESPONSE_SECONDS:
             asyncio.sleep(self.POLLING_INTERVAL_SECONDS)  # Ratelimiting
             message_status = await self.sendread_nolock(message)
-        # If the timeout is exceeded and we're still getting NACKs, let the
-        # caller know so they can do recovery.
+        if message_status == non_response_reply:
+            raise NonResponseError(message, self.__class__.__name__)
         return message_status
 
     async def sendread_until_data_or_nack(self, message: str) -> str:
@@ -137,7 +150,6 @@ class BillValidator(Peripheral):
 
     @staticmethod
     def create_address_byte(command: str) -> str:
-        assert command in BillValidator.COMMANDS
         combined_hex = BillValidator.ADDRESS | BillValidator.COMMANDS[command]
         return f"{combined_hex:x}"
 
@@ -152,32 +164,106 @@ class BillValidator(Peripheral):
             logger.critical('Unable to start the bill validator.')
             raise
 
-    async def reset(self, send_reset=True) -> None:
+    async def reset(self, send_reset=True, poll_reset=True) -> None:
         """Resets the bill validator.
 
         Will only return once the validator is finished the reset command
         sequence; should be wrapped with asyncio.wait_for if you want to
-        timeout the attempts."""
+        timeout the attempts.
+
+        :param send_reset: Whether to send a reset command.
+        :param poll_reset: Whether to poll for a JUST RESET."""
         with self.lock:
             while True:
-                if send_reset:
-                    command = "R," + self.create_address_byte('RESET') + "\n"
-                    response = 'R,NACK'
-                    while response == 'R,NACK':
+                try:
+                    if send_reset:
+                        command = "R," + self.create_address_byte('RESET') + \
+                                "\n"
                         logger.info('Sending reset command to bill validator.')
-                        response = await self.sendread_nolock_until_timeout(
-                            command)
-                    await asyncio.sleep(SETUP_TIME_SECONDS)
-                # Poll until JUST RESET
-                logger.info('Polling bill validator for JUST RESET.')
-                response = await self.sendread_nolock_until_data_or_nack(
-                    self.POLL_COMMAND)
-                if response == 'R,NACK':
-                    logger.error("Exceeded non-response time during reset, "
-                                 "check if the vending machine is on fire.")
-                    return False
-                response_statuses = [response[i:i+2] for
-                                     i in range(2, len(response), 2)]
+                        # Only responses are ACK or NACK, and this will throw
+                        # a NonResponseError if all we get is NACK.
+                        await self.sendread_nolock_until_timeout(command)
+                        await asyncio.sleep(SETUP_TIME_SECONDS)
+                    # Poll until JUST RESET
+                    if poll_reset:
+                        logger.info('Polling bill validator for JUST RESET.')
+                        response = \
+                            await self.sendread_nolock_until_data_or_nack(
+                                self.POLL_COMMAND)
+                        response_statuses = [response[i:i+2] for
+                                             i in range(2, len(response), 2)]
+                        # 0x06 is the code for JUST RESET.
+                        if '06' not in response_statuses:
+                            logger.warning("Did not get JUST RESET in the "
+                                           "first poll after resetting, trying"
+                                           "again.")
+                            send_reset = True
+                            # poll_reset = True
+                            continue
+                        # Pass off the remaining responses to the poll handler
+                        # in the background.
+                        asyncio.create_task(self.handle_poll_responses(
+                            [x for x in response_statuses if x != '06']))
+
+                    logger.info('Getting bill validator setup information.')
+                    setup_data = await self.sendread_nolock_until_data_or_nack(
+                        "R," + self.create_address_byte('SETUP') + "\n")
+                    setup_data_bytes = [setup_data[i:i+2] for
+                                        i in range(2, len(setup_data), 2)]
+                    self.feature_level = int(setup_data_bytes[0], base=16)
+                    logger.debug(f'Bill validator level: {self.feature_level}')
+                    country_code = setup_data_bytes[1] + setup_data_bytes[2]
+                    if country_code != '0001' and country_code != '1840':
+                        raise RuntimeError('Bill validator does not use USD, '
+                                           'stated country code is '
+                                           f'{country_code}.')
+                    # How many cents a single 'unit' of value corresponds to.
+                    self.scaling_factor = int(setup_data_bytes[3] +
+                                              setup_data_bytes[4], base=16)
+                    self.stacker_capacity = int(setup_data_bytes[6] +
+                                                setup_data_bytes[7], base=16)
+                    self.security_level_bitvector = int(setup_data_bytes[8] +
+                                                        setup_data_bytes[9],
+                                                        base=16)
+                    # I don't know whether it uses caps or not for hex.
+                    self.has_escrow = setup_data_bytes[10] != '00'
+                    self.bill_values = [int(x, base=16) for x in
+                                        setup_data_bytes[11:]]
+
+                    expansion_command = 'R,' + \
+                        self.create_address_byte('EXPANSION')
+                    if self.feature_level == 1:
+                        expansion_command += ',00\n'
+                    else:
+                        expansion_command += ',02\n'
+                    logger.info('Getting bill validator expansion '
+                                'information.')
+                    self.expansion_data = \
+                        await self.sendread_nolock_until_data_or_nack(
+                            expansion_command)
+                    logger.info('Got expansion data for bill validator: '
+                                f'{self.expansion_data}')
+
+                    logger.info('Getting stacked bill count.')
+                    stacker_count = \
+                        await self.sendread_nolock_until_data_or_nack(
+                            'R,' + self.create_address_byte('STACKER') + '\n')
+                    stacker_count = int(stacker_count[2:], base=16)
+                    self.stacker_count = (~0x8000) & stacker_count
+                    self.stacker_full = stacker_count >= 0x8000
+
+                    bills_to_enable = [int(x > 0 and x < 0xff) for x in
+                                       self.bill_values]
+                    bills_to_enable.extend([0] * (16 - len(self.bill_values)))
+                    # TODO: Figure out how this should be converted into a
+                    # bitvector. It's not clear if I need to reverse the list
+                    # before doing the shift-and-add.
+                except NonResponseError as e:
+                    logger.warning("Bill validator timed out while resetting, "
+                                   f"command was '{e.command}'.")
+                    send_reset = True
+                    poll_reset = True
+                    continue
 
     async def handle_poll_responses(self, responses: Sequence[str]):
         pass
